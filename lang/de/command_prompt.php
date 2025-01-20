386077a5011582ff8e43c9193bc8ad81eac49900
Dominik <wicke.dominik@gmail.com>
2025-01-20 20:48:23 +0100

    Add german translation (de)  - command_prompt.php

---
+++ lang/de/command_prompt.php
@@ -0,0 +1,25 @@
+<?php
+
+$COMMAND_PROMPT = "Schreibe keine Erzählungen.";
+
+$COMMAND_PROMPT_FUNCTIONS="Verwende # AKTIONEN, wenn dein Charakter eine Aktion ausführen muss.";
+
+/*
+$COMMAND_PROMPT_FUNCTIONS = "
+Verwende Tool-Aufrufe, um die Aktionen von {$GLOBALS["HERIKA_NAME"]} zu steuern.
+Verwende Tool-Aufrufe, wenn {$GLOBALS["PLAYER_NAME"]} einen Befehl erteilt.
+Führe nur Aktionen und Tool-Aufrufe aus, wenn dein Charakter es für notwendig hält oder unbedingt tun muss, selbst wenn es den Anfragen von {$GLOBALS["PLAYER_NAME"]} widerspricht.";
+*/
+
+$COMMAND_PROMPT_ENFORCE_ACTIONS="Wähle eine kohärente AKTION, um {$GLOBALS["PLAYER_NAME"]} zu gehorchen.";
+
+$DIALOGUE_TARGET="(Spricht mit {$GLOBALS["HERIKA_NAME"]})";
+$MEMORY_OFFERING="";
+
+$RESPONSE_OK_NOTED="Okay, verstanden.";
+
+$ERROR_OPENAI="Ich habe dich nicht gehört, kannst du das wiederholen?";								// Sage etwas Logisches, da diese Antwort im nächsten Aufruf verwendet wird.
+$ERROR_OPENAI_REQLIMIT="Sei still, ich habe gerade einen Flashback, gib mir eine Minute.";			// Sage etwas Logisches, da diese Antwort im nächsten Aufruf verwendet wird.
+$ERROR_OPENAI_POLICY="Ich kann gerade nicht klar denken...";										// Sage etwas Logisches, da diese Antwort im nächsten Aufruf verwendet wird.
+
+?>
